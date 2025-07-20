c2a6eed71d592d64d40738f1738fa21f915f7b30
md-shafi-ur-rehaman <mohammadshafiq.rahaman@gmail.com>
2025-07-20 16:05:47 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import prisma from '@/lib/db';
-
+import { Suspense } from 'react';
+import { getQueryClient, trpc } from '@/trpc/server';
+import {  HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import Client from "@/app/client"
 export default async function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"Shafi"}))
+
   return (
-    <div>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loding...</p>}>
+        <Client/>
+      </Suspense>
+    </HydrationBoundary>
   );
 }
