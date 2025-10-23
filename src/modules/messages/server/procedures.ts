49817105e1cd13736a2b33f75e263cb7fd21a44d
md-shafi-ur-rehaman <mohammadshafiq.rahaman@gmail.com>
2025-10-23 17:54:59 +0530

    Authentication

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -1,19 +1,23 @@
 import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import {z} from "zod";
 
 export const messageRouter = createTRPCRouter({
-    getMany: baseProcedure
+    getMany: protectedProcedure
         .input(
             z.object({
                 projectId: z.string().min(1, {message:"Project Id is required"})
             })
         )
-        .query(async ({input}) => {
+        .query(async ({input, ctx}) => {
             const messages = await prisma.message.findMany({
                 where:{
                     projectId:input.projectId,
+                    project:{
+                        userId: ctx.auth.userId 
+                    }
                 },
                 include:{
                     fragment:true,
@@ -24,17 +28,28 @@ export const messageRouter = createTRPCRouter({
             });
             return messages;
         }),
-    create: baseProcedure
+    create: protectedProcedure
         .input(
             z.object({
                 value: z.string().min(1, {message:"Message is required"}),
                 projectId: z.string().min(1, {message:"Project Id is required"})
             })
         )
-        .mutation( async ({input}) =>{
+        .mutation( async ({input, ctx}) =>{
+            const ExistingProject = await prisma.project.findUnique({
+                where:{
+                    id:input.projectId,
+                    userId: ctx.auth.userId
+                }
+            })
+
+            if(!ExistingProject){
+                throw new TRPCError({code:"NOT_FOUND", message:"Project not found"})
+            }
+
             const createdMessage = await prisma.message.create({
                 data:{
-                    projectId:input.projectId,
+                    projectId:ExistingProject.id,
                     content:input.value,
                     role: "USER",
                     type: "RESULT"
